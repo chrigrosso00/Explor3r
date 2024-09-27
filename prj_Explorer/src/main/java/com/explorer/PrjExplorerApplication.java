80ca3c3212c6acf0463fad0d6049d4e39785ed2b
PinnaFrancesco <162339009+PinnaFrancesco@users.noreply.github.com>
2024-09-27 14:33:35 +0200

    Add files via upload

---
+++ prj_Explorer/src/main/java/com/explorer/PrjExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjExplorerApplication.class, args);
+	}
+
+}
